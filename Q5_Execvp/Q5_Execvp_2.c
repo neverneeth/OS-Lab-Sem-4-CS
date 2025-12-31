c058a8fb95a9760a99a6136480d42aae3d1ae2d4
Joel Lijo Mathew <joellijo4@gmail.com>
2025-12-31 22:28:46 +0530

    completed 5th question, verification pending

--- Q5_Execvp/Q5_Execvp_2.c
+++ Q5_Execvp/Q5_Execvp_2.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/types.h>
 
 int main(){
-    printf("\nThis is the second program.\n"); 
+    pid_t pid;
+    pid = fork();
+    if(pid == 0){
+        
+        char *args[] = {"./myadder", "10", "20", NULL}; 
+        execvp(args[0], args);
+    } else{
+        printf("\nThis is the second program.\n"); 
+    }
     
+
+
     return 0;
 }
