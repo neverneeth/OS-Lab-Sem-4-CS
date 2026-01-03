8132ea4b449730cdb759fa2c1c36a259779fa945
Joel Lijo Mathew <joellijo4@gmail.com>
2026-01-03 11:21:44 +0000

    Implemented the mean thread for question 8, two remaining

--- Q8_Multithreading/Q8_Multithreading.c
+++ Q8_Multithreading/Q8_Multithreading.c
@@ -1,2 +1,19 @@
 #include <stdio.h>
 #include <pthread.h>
+
+double mean,std_dev;  int median;
+
+typedef struct Thread{
+	int *array; 
+	int size;
+} Thread;
+
+
+void *calculate_mean(void *args){
+	Thread *mean_args = (Thread *)args;
+	for(int i = 0; i< mean_args->size; i++){
+		mean = mean + (mean_args->array[i]); 
+	}
+	mean = mean / (mean_args->size); 
+	return mean_args; 
+}
