42dca8b858f14f1f5b672dec85615a751a1b39c8
Joel Lijo Mathew <joellijo4@gmail.com>
2026-01-03 11:40:56 +0000

    Completed the median implementation for question 8, one more to go working completely fine so far

--- Q8_Multithreading/Q8_Multithreading.c
+++ Q8_Multithreading/Q8_Multithreading.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <stdlib.h>
 
-double mean,std_dev;  int median;
+
+double mean,std_dev, median;
 
 typedef struct Thread{
 	int *array; 
@@ -16,4 +18,31 @@ void *calculate_mean(void *args){
 	}
 	mean = mean / (mean_args->size); 
 	return mean_args; 
+}
+
+
+void *calculate_median(void *args){
+	Thread *median_args = (Thread *)args;
+	int *ar = (int *)malloc((median_args->size)* sizeof(int));
+	for(int i = 0; i < median_args->size; i++){
+		ar[i] = median_args->array[i]; 
+	}
+	for(int i  = 0; i < (median_args->size - 1); i++){
+		int small = ar[i], pos = i;
+		for(int j = i+1; j < (median_args->size); j++){
+			if(ar[j] < small){
+				small = ar[j]; pos = j;
+			}
+		}
+		ar[pos] = ar[i];
+		ar[i] = small;
+	}
+	if((median_args->size)%2 == 0){
+		double mid = median_args->size / 2;
+		median = (ar[mid] + ar[mid-1]) /2 ;
+	} else{
+		median = ar[(median_args->size)/2] ;
+	}
+	free(ar); 
+	return median_args;
 }
