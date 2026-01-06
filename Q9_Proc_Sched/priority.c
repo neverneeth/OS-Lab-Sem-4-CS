9c5c121c367a62285ea21f67bb4e88df496184f8
Joel Lijo Mathew <joellijo4@gmail.com>
2026-01-06 14:03:37 +0000

    Gantt chart issues need to be resolved - q9

--- Q9_Proc_Sched/priority.c
+++ Q9_Proc_Sched/priority.c
@@ -19,20 +19,42 @@ int main(){
 	}
 	int current_time = 0, wait_sum = 0, tat_sum = 0;
 	printf("\n\nNon-Preemptive Priority Scheduling: \n");
-
+	
 
 	for(int i = 0;i < n;i++){
 		p[i].is_complete = 0;
 	}
-	/*printf("\nNon-Preemptive Priority Scheduling Complete.\n");
-	printf("\nAverage Waiting Time: %lf", (double)wait_sum/n);
-	printf("\nAverage Turn Around Time: %lf", (double)tat_sum/n);
+	int completed = 0;
+	while(completed < n){
+		int max_prio = -1, index = -1;
+		for(int i = 0;i < n;i++){
+			if(p[i].arrival <= current_time && p[i].is_complete == 0){
+				if(p[i].priority > max_prio){
+					max_prio = p[i].priority, index = i;
+				}
+			}
+		}
+		if(index == -1){
+			current_time++;continue;
+		}
+		p[index].completion = current_time + p[index].burst;
 
-	printf("\n\nGantt chart for Non-Preemptive Priority: \n");
-	for(int i = 0; i < n; i++){
-		printf(" | %d", p[i].id);
-	}printf(" |\n");
-	*/
+       	 	p[index].turnaround = p[index].completion - p[index].arrival;
+        	tat_sum += p[index].turnaround;
 
-	return 0;
+        	p[index].waiting = p[index].turnaround - p[index].burst;
+        	wait_sum += p[index].waiting;
+
+		p[index].is_complete = 1; completed++;
+	}
+        printf("\nNon-Preemptive Priority Scheduling Complete.\n");
+        printf("\nAverage Waiting Time: %lf", (double)wait_sum/n);
+        printf("\nAverage Turn Around Time: %lf", (double)tat_sum/n);
+
+        printf("\n\nGantt chart for Non-Preemptive Priority: \n");
+        for(int i = 0; i < n; i++){
+            	printf(" | %d", p[i].id);
+        }printf(" |\n");
+
+        return 0;
 }
