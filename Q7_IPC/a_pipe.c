d73e9094954cba0be501a70ffc6539800d83fb12
Joel Lijo Mathew <joellijo4@gmail.com>
2026-01-13 18:00:26 +0000

    Completed q7 - a) ipc

--- Q7_IPC/a_pipe.c
+++ Q7_IPC/a_pipe.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <math.h>
 
 int main(){
 
@@ -25,7 +26,29 @@ int main(){
 		return 1;
 	}
 
+	if(pid == 0){
+		close(fd[0]);
+		int output = 4 * a * c ;
+		printf("\nSecond Process Calculated 4ac: %d", output);
 
+		write(fd[1], &output, sizeof(int));
+		close(fd[1]);
+
+		return 0;
+	} else {
+		close(fd[1]) ;
+		printf("\nParent Process Calculated b2: %d", b*b);
+
+		int recieved ;
+		recieved = read(fd[0], &recieved, sizeof(int)); 
+		printf("\nParent Recieved 4ac: %d", recieved);
+		close(fd[0]);
+
+		int result = sqrt((b*b) - recieved) ;
+		printf("\nFinal Result : %d\n", result);
+		wait(NULL);
+
+	}
 
 
 
