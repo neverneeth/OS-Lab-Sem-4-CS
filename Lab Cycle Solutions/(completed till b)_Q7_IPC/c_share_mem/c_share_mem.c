22c31a4aaa58e00d3197b5438c2f58f72c905544
Joel Lijo Mathew <joellijo4@gmail.com>
2026-02-15 13:03:06 +0530

    Q7 c Started working on- included header files

---
+++ Lab Cycle Solutions/(completed till b)_Q7_IPC/c_share_mem/c_share_mem.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/shm.h>
+#include <ctype.h>
+#include <sys/ipc.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
