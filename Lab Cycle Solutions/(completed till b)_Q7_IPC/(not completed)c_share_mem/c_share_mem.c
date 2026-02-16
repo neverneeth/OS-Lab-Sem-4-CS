e61254a8fa288f37d63e6fbab528a653edf17ede
Joel Lijo Mathew <joellijo4@gmail.com>
2026-02-16 20:38:54 +0530

    Q7 c shared memory intialised

--- Lab Cycle Solutions/(completed till b)_Q7_IPC/(not completed)c_share_mem/c_share_mem.c
+++ Lab Cycle Solutions/(completed till b)_Q7_IPC/(not completed)c_share_mem/c_share_mem.c
@@ -19,4 +19,16 @@ typedef struct {
 
 int main(){
 	key_t key = ftok("shmfile", 65);
+
+	int shmid = shmget(key, sizeof(SharedData), 0666 | IPC_CREAT);
+
+	if(shmid == -1){
+		printf("\nSharing memory failed !\n");
+		return 1;
+	}
+
+	SharedData *data = (SharedData *) shmat(shmid, (void *)0, 0);
+	data->status = 0;
+
+	
 }
